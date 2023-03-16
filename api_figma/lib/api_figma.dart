bc17572a6e1bd87a18dca90fee07aabbe78af2e5
Flavio Aro <flavio.aro@gmail.com>
2023-03-16 17:03:57 -0300

    refactor all

--- api_figma/lib/api_figma.dart
+++ api_figma/lib/api_figma.dart
@@ -2,17 +2,23 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  final String url = "https://api.figma.com/v1/files/ID OF YOUR FIGMA FILE";
-  final String token = "TOKEN";
+  final String idFigma = "56556dfnjhud67nfkd87";
+  final String personalAccessToken =
+      "fijaidjf843ju8j3843984jjjshundswtf4ydsjfgqkipd";
 
   final response = await http.get(
-    Uri.parse(url),
-    headers: {"X-Figma-Token": token},
+    Uri.parse("https://api.figma.com/v1/files/$idFigma"),
+    headers: {"X-Figma-Token": personalAccessToken},
   );
 
   if (response.statusCode == 200) {
     final jsonResponse = json.decode(response.body);
-    print(jsonResponse);
+    // List<String> responseArray = jsonResponse.toString().split(RegExp(r'[:,]'));
+    List<String> responseArray = jsonResponse.toString().split(",");
+
+    for (int i = 0; i < responseArray.length; i++) {
+      print("Counter: ($i) = ${responseArray[i]}");
+    }
   } else {
     print("Request failed with status: ${response.statusCode}.");
   }
